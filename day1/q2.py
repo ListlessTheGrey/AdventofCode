f7cdac6e24934756aa84c9ae3db4360f26606273
Callum S <25096397@edgehill.ac.uk>
2021-12-01 17:31:09 +0000

    Update q2.py

--- day1/q2.py
+++ day1/q2.py
@@ -5,8 +5,15 @@ def three():
     inputlist = [int(x) for x in inputlist]
     increasecount = 0
     threelist = []
-    length = len(threelist)
+    length = len(inputlist)
     for i in range(length):
+        try:
+            threesum = inputlist[i] + inputlist[i + 1] + inputlist[i + 2]
+            threelist.append(threesum)
+        except:
+            break
+    threelength = len(threelist)
+    for i in range(threelength):
         if threelist[i - 1] < threelist[i]:
             increasecount += 1
         else:
@@ -16,3 +23,4 @@ def three():
 
 if __name__ == "__main__":
     print(three())
+    
