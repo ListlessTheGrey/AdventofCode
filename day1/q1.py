505d48c30a1a648be74bae2952ebec8042b95209
Callum S <25096397@edgehill.ac.uk>
2021-12-01 16:55:24 +0000

    Update q1.py

--- day1/q1.py
+++ day1/q1.py
@@ -1,12 +1,20 @@
-with open("day1/input.txt", "r") as f:
-    inputstring = f.read()
-inputlist = inputstring.split()
-inputlist = [int(x) for x in inputlist]    
-increasecount = 0
-length = len(inputlist)
-for i in range(length):
-    if inputlist[i - 1] < inputlist[i]:
-        increasecount += 1
-    else:
-        pass
-print(increasecount)
+def list():    
+    with open("day1/input.txt", "r") as f:
+        inputstring = f.read()
+    inputlist = inputstring.split()
+    inputlist = [int(x) for x in inputlist]    
+    increasecount = 0
+    length = len(inputlist)
+    for i in range(length):
+        if inputlist[i - 1] < inputlist[i]:
+            increasecount += 1
+        else:
+            pass
+    return increasecount
+
+def main():
+    print(list())
+
+if __name__ == "__main__":
+    main()
+    
